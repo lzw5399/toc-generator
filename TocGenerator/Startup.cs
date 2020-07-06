3c3d00ffbde0e7a0b07d113df42e0c8fc00a13a5
zhiwen <lzw5399@gmail.com>
2020-07-06 13:29:05 +0800

    chore: remove HSTS setting

--- TocGenerator/Startup.cs
+++ TocGenerator/Startup.cs
@@ -20,13 +20,11 @@ namespace TocGenerator
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,15 +34,11 @@ namespace TocGenerator
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
             app.UseStaticFiles();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
